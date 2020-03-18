763d9d322e9a23f2d37c6e375a1d1c3b6a4fac13
riversun <riversun.org@gmail.com>
2020-03-18 23:17:23 +0900

    font awesome branch first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './index.scss';
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import { faDog, faCat } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faDog, faCat);
+
+dom.i2svg();
 
 export { default } from './sortable-table';
